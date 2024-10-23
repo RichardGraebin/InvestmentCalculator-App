918f965b5fca22739117b9b4d3b6d6283c03c90e
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-10-23 09:44:04 -0300

    header, main section and table html structure implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
+import logoPath from "./assets/investment-calculator-logo.png";
+
 function App() {
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <header id="header">
+        <img src={logoPath} />
+        <h1>Investment Calculator</h1>
+      </header>
+      <section id="user-input">
+        <div className="input-group">
+          <label>
+            <p>Initial Investment</p>
+            <input type="number" />
+          </label>
+          <label>
+            <p>Annual Investment</p>
+            <input type="number" />
+          </label>
+        </div>
+        <div className="input-group">
+          <label>
+            <p>Expected Return</p>
+            <input type="number" />
+          </label>
+          <label>
+            <p>Duration</p>
+            <input type="number" />
+          </label>
+        </div>
+      </section>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Investment Value</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th></th>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
 }
 
-export default App
+export default App;
