7a7476a72a8fe7efca3d898564229172f96a6db1
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-10-27 15:15:56 -0300

    implemented userinput data manipulation and rendering

--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,24 +1,51 @@
+import { useState } from "react";
+
 export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 12000,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevInputs) => {
+      return { ...prevInputs, [inputIdentifier]: newValue };
+    });
+  }
+
   return (
     <section id="user-input">
       <div className="input-group">
         <label>
           <p>Initial Investment</p>
-          <input type="number" />
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
+          />
         </label>
         <label>
           <p>Annual Investment</p>
-          <input type="number" />
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
+          />
         </label>
       </div>
       <div className="input-group">
         <label>
           <p>Expected Return</p>
-          <input type="number" />
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={(e) => handleChange("expectedReturn", e.target.value)}
+          />
         </label>
         <label>
           <p>Duration</p>
-          <input type="number" />
+          <input type="number" value={userInput.duration} onChange={(e) => handleChange("duration", e.target.value)} />
         </label>
       </div>
     </section>
